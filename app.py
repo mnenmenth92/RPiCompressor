7eea585ce8d69d521b387b1696b198bb43354b7b
unknown <msuch92@gmail.com>
2020-09-05 10:28:40 +0200

    pressure.html, flask server started.

--- app.py
+++ app.py
@@ -3,21 +3,27 @@ from analog_input import AnalogInput
 from rpi_compressor import RpiCompressor
 from config import pressure_check_time_interval
 from time import sleep
+from flask import Flask
+from flask import render_template
 
 gpio_driver = GPIODriver()
 analog_input = AnalogInput()
 rpi_compressor = RpiCompressor(analog_input, gpio_driver)
 
+app = Flask(__name__)
 
-
-
+@app.route('/Pressure')
+def index():
+    return render_template('pressure.html',
+                           current_pressure=analog_input.current_value,
+                           target_pressure=rpi_compressor.pressure_limit)
 
 
 
 if __name__ == '__main__':
     try:
-        # ToDo run flask server
-        # TEMPORARY
+        app.run(debug=True)
+        # ToDo check how to run while loop along with flask server
         while True:
             rpi_compressor.manage_pressure()
             sleep(pressure_check_time_interval)
